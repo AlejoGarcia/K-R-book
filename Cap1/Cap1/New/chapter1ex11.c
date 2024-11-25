f43661bcb38b858736c6df3a26d8ad41ddfebaa0
Alejo García Galán <alejogarciagalan@gmail.com>
2024-11-26 00:17:17 +0100

    Fixed bad copy of merged files

--- Cap1/Cap1/New/chapter1ex11.c
+++
@@ -1,4 +0,0 @@
-/*
- * I would test it with a variaiety of inputs and count each letter to check if it keeps count properly. The same with words and lines.
- * Would also put several tabs and spaces between words to check if it parses them correctly.
- */
--- Cap1/Cap1/New/chapter1ex7.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-/* Prints value of EOF */
-main()
-{
-	printf("EOF value: %d\n", EOF); 
-}
